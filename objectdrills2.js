c2e00e54324522bb4378f0cc8e05e3a4739e9718
nfjones87 <nfjones87@gmail.com>
2017-05-10 12:16:48 -0700

    drills3

--- objectdrills2.js
+++ objectdrills2.js
@@ -61,3 +61,51 @@ function findById(items, idNum) {
 }
 
 
+-------------------------------------------------------
+
+
+function validateKeys(object, expectedKeys) {
+  
+  // if there's not the same number of object keys
+  // and expected keys, then we know there are missing or
+  // extra keys, so return false
+  if (Object.keys(object).length !== expectedKeys.length) {
+    return false;
+  }
+  
+  // we iterate over each expected key and verify that
+  // it's found in `object`.
+  for (var i; i<expectedKeys.length; i++) {
+    if (Object.keys(object).find(function(key) {
+      return key !== expectedKeys[i];
+    })) {
+      return false;
+    }
+  }
+  // if we get to this point in our code, the keys are valid
+  // so we return `true`
+  return true;
+}
+
+
+-------------------------------------------------------------
+
+function makeToDos(owner, toDos) {
+  return {
+    owner: owner,
+    toDos: toDos,
+    generateHtml: function() {
+      var html = '<ul>';
+      this.toDos.forEach(function(todo) {
+        html+= '<li>' + todo + '</li>';
+      });
+      return html + '</ul>';
+    }
+  }
+}
+
+
+------------------------------------------------------------
+
+
+
---
+++ objectdrills3.js
@@ -0,0 +1,17 @@
+let loaf = {
+	flour: 300,
+	water: 210,
+	hydration: function(){
+		return (this.water/this.flour)*100;
+	}
+}
+
+console.log(loaf);
+
+loaf.hydration();
+
+-------------------------------------------------
+
+
+
+
