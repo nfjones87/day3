4bbf2ff9708db80ca57c162ae0ab2fb4c66d397c
nfjones87 <nfjones87@gmail.com>
2017-05-10 12:43:45 -0700

    another commit

--- objectdrills3.js
+++ objectdrills3.js
@@ -12,6 +12,61 @@ loaf.hydration();
 
 -------------------------------------------------
 
+const stuff = {
+	foo: 1,
+	bar: 2,
+	fum: 3,
+	quux: 4,
+	spam: 5
+};
+
+for (let key in stuff) {
+	console.log(key, stuff[key]);
+}
+
+---------------------------------------------------
+
+const hobbit = {
+	meals: ["breakfast", "second breakfast", "lunch", "tea", "dinner", "supper"]
+}
+console.log(hobbit.meals[3]);
+
+--------------------------------------------------------
+
+let array = [
+
+{name: "Nick", job: "developer"},
+
+{name: "Bob", job: "writer" },
+
+{name: "carl", job: "cook"}
+
+];
+
+for (i=0;i<array.length;i++) {
+	let employer = array[i]
+	console.log(employer.name + " is the " + employer.job);
+};
+
+-------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
