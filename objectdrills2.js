e0827081e933fe30f42ad53fefdc752303e19d22
nfjones87 <nfjones87@gmail.com>
2017-05-10 11:28:04 -0700

    drills2

--- objectdrills2.js
+++ objectdrills2.js
@@ -5,12 +5,59 @@ function makeStudentsReport(data) {
 }
 
 
+-----------------------------------------------------------
+
 function makeStudentsReport(data) {
 	let results = [];
 	for (i=0; i<data.length;i++) {
 		let item = data[i];
 	results.push(item.name + ": " + item.grade);
-  }
-return results;
+    }
+    return results;
 }
+
+------------------------------------------------------------
+
+
+var studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology'
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics'
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science'
+  }
+];
+
+function enrollInSummerSchool(students) {
+	return students.map(function(enrollment){
+		return {
+			name: enrollment.name,
+			status: 'In Summer school',
+			course: enrollment.course,
+		}
+	});
+}
+
+
+
+-----------------------------------------------------
+
+
+
+
+function findById(items, idNum) {
+ return items.find(function(item){
+ 	return item.id===idNum;
+ });
+}
+
 
